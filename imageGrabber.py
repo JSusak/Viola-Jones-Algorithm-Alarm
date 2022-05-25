472d5988ab3cef50ea993cc27157300f3851ed3f
Josh <joshuasusak@gmail.com>
2022-05-25 09:55:03 +0100

    Cleaned indentation.

--- imageGrabber.py
+++ imageGrabber.py
@@ -1,8 +1,10 @@
 import cv2
 import time
+
 currentWebcam = cv2.VideoCapture(0)
+
 
 def takePicture(frame):
     image = "picture_{}.png".format(time.time())
-    cv2.imwrite('imageLogs/'+image, frame)
+    cv2.imwrite("imageLogs/" + image, frame)
     print("Image saved.")
--- soundThreader.py
+++ soundThreader.py
@@ -1,25 +1,24 @@
-#CODE IS HERE if you want to run a function EXACTLY once throughout the duration of the loop.
+# CODE IS HERE if you want to run a function EXACTLY once throughout the duration of the loop.
 
-#import threading
-#import queue
-#import winsound
-#from cv2 import add
-#from time import sleep
+# import threading
+# import queue
+# import winsound
+# from cv2 import add
+# from time import sleep
 from playsound import playsound
 
-#q = queue.Queue()
-#def runOnce(f):
-        #async def wrapper(*args, **kwargs):
-                #if not wrapper.has_run or wrapper.run_again:
-                        #wrapper.has_run = True
-                        #await asyncio.sleep(3)
-                        #return f(*args, **kwargs)
+# q = queue.Queue()
+# def runOnce(f):
+# async def wrapper(*args, **kwargs):
+# if not wrapper.has_run or wrapper.run_again:
+# wrapper.has_run = True
+# await asyncio.sleep(3)
+# return f(*args, **kwargs)
 
-        #wrapper.has_run = False
-        #wrapper.run_again = True
-        #return wrapper
+# wrapper.has_run = False
+# wrapper.run_again = True
+# return wrapper
 
-#Asynchronously play a sound in the resources folder.
-def playSound(sound,extension):
-        playsound("resources/"+sound+"."+extension,block=False)
-
+# Asynchronously play a sound in the resources folder.
+def playSound(sound, extension):
+    playsound("resources/" + sound + "." + extension, block=False)
