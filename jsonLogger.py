8c69eff21e8798187a9c15bc87c784d9db35fe7e
Josh <joshuasusak@gmail.com>
2022-05-25 09:03:32 +0100

    Added ability to read logs.

--- jsonLogger.py
+++ jsonLogger.py
@@ -17,3 +17,28 @@ def replaceLogs(detect):
 
     with open("jsonLogs/detectionlogs.json", "w") as f:
         json.dump(data, f)
+
+
+def readLogs():
+    msg = ""
+    msg += "----------CURRENT JSON STATS----------" + "\n"
+    with open("jsonLogs/detectionlogs.json", "r") as logFile:
+        data = json.load(logFile)
+        for item in data["lastDetectedLog"]:
+            msg += (
+                "• Last detected position was at: "
+                + str(item["DetectionPositions"])
+                + ".\n"
+            )
+            msg += (
+                "• The last point of time at which a face was detected was at: "
+                + str(item["LastDetectedTime"])
+                + ".\n"
+            )
+            msg += (
+                "• A face has been detected "
+                + str(item["timesDetected"])
+                + "times. Not very sneaky :)\n"
+            )
+
+    return msg
