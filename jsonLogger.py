c775bab0e510e03dfbab7c0863f5ea5301b53c5e
Josh <joshuasusak@gmail.com>
2022-05-25 08:31:21 +0100

    Made code look prettier.

--- jsonLogger.py
+++ jsonLogger.py
@@ -1,13 +1,19 @@
 import json
 from datetime import datetime
 
+
 def replaceLogs(detect):
-    with open('jsonLogs/detectionlogs.json') as logFile:
+    with open("jsonLogs/detectionlogs.json") as logFile:
         data = json.load(logFile)
-        for item in data['lastDetectedLog']:
-            item["DetectionPositions"] = item["DetectionPositions"].replace(str(item["DetectionPositions"]), str(detect))
-            item["LastDetectedTime"] = item["LastDetectedTime"].replace(str(item["LastDetectedTime"]), str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+        for item in data["lastDetectedLog"]:
+            item["DetectionPositions"] = item["DetectionPositions"].replace(
+                str(item["DetectionPositions"]), str(detect)
+            )
+            item["LastDetectedTime"] = item["LastDetectedTime"].replace(
+                str(item["LastDetectedTime"]),
+                str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")),
+            )
             item["timesDetected"] = item["timesDetected"] + 1
 
-    with open('jsonLogs/detectionlogs.json', 'w') as f:
+    with open("jsonLogs/detectionlogs.json", "w") as f:
         json.dump(data, f)
