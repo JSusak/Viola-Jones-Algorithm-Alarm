c12326a3ca0270e471c85ce6005a813374307548
Josh <joshuasusak@gmail.com>
2022-05-29 19:22:02 +0100

    Added new constants file.

---
+++ CONSTANTS.py
@@ -0,0 +1,8 @@
+#All constants related to both webcam and phone operations can be found here.
+
+# Change this value to modify the interval at which frames are inspected.
+INTERVAL = 30
+
+# Change this variable to modify the universal colour of all shapes in the program. You can also choose to leave it as None, providing random colours.
+# Put in the form (g,b,r) otherwise program will crash!
+COLOUR = (0, 23, 250)
