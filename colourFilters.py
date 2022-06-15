d6bcab6122225efb3d02b12fb00e09d1cc6f8e28
Josh <joshuasusak@gmail.com>
2022-06-15 11:58:35 +0100

    Added sepia image.

--- colourFilters.py
+++ colourFilters.py
@@ -2,3 +2,9 @@ import cv2
 def invertImage(frame):
     print("Image inverted...")
     return cv2.bitwise_not(frame);
+
+def sepiaImage(frame):
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2BGRA)
+    frame_h,frame_w,frame_c = frame.shape
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGRA2BGR)
+    return frame
